b84c8cc49492965d5f6722d8cde6e306874b741d
otsuka.amane <otsuka.amane@gmail.com>
2020-04-25 16:03:13 +0900

    素数判定

--- Prime/ViewController.swift
+++ Prime/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var inputNumberTextField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    
     override func viewDidLoad() {
+        inputNumberTextField.delegate = self
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @objc func prime() {
+        print(inputNumberTextField)
+        print("test")
+        let convertNumber = Int(inputNumberTextField.text!)
+        var sqrtValue = sqrt(Double(convertNumber!))
+        var floorValue = floor(sqrtValue)
+        var IntValue = Int(floorValue)
+        for i in 2..<IntValue {
+            
+            if convertNumber! % i == 0 {
+                print("素数じゃない")
+                outputLabel.text = "\(inputNumberTextField.text!)は素数じゃない"
+                outputLabel.textColor = UIColor.white
+                return
+            }
+        }
+        outputLabel.text = "\(inputNumberTextField.text!)は素数"
+        return
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textfieldEndEditing")
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesbegan")
+        view.endEditing(true)
+        prime()
+    }
 }
 
